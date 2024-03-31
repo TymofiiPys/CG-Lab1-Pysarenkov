3fc4ea9eec79519c4e4e02b6a855edd7036f24fe
TymofiiPys <71066289+TymofiiPys@users.noreply.github.com>
2024-03-31 22:06:00 +0300

    Updated Comparators for Edges, adapted to new Graph code

--- src/main/java/org/example/pointloc/EdgeDestXComparator.java
+++ src/main/java/org/example/pointloc/EdgeDestXComparator.java
@@ -1,13 +1,36 @@
 package org.example.pointloc;
 
-import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import java.util.Comparator;
 
-public class EdgeDestXComparator implements Comparator<Edge> {
+//public class EdgeDestXComparator implements Comparator<Edge> {
+//    @Override
+//    public int compare(Edge o1, Edge o2) {
+//        GraphNode o1point = o1.getDest();
+//        GraphNode o2point = o2.getDest();
+//        if(o1point.getX() > o2point.getX())
+//            return 1;
+//        if(o1point.getX() < o2point.getX())
+//            return -1;
+//        if(o1point.getY() > o2point.getY())
+//            return 1;
+//        if(o1point.getY() < o2point.getY())
+//            return -1;
+//        return 0;
+//    }
+//}
+
+public class EdgeDestXComparator implements Comparator<Integer> {
+    private final ArrayList<WeightedEdge> edges;
+
+    public EdgeDestXComparator(ArrayList<WeightedEdge> edges) {
+        this.edges = edges;
+    }
+
     @Override
-    public int compare(Edge o1, Edge o2) {
-        GraphNode o1point = o1.getDest();
-        GraphNode o2point = o2.getDest();
+    public int compare(Integer o1, Integer o2) {
+        GraphNode o1point = edges.get(o1).getDest();
+        GraphNode o2point = edges.get(o2).getDest();
         if(o1point.getX() > o2point.getX())
             return 1;
         if(o1point.getX() < o2point.getX())
--- src/main/java/org/example/pointloc/EdgeSrcXComparator.java
+++ src/main/java/org/example/pointloc/EdgeSrcXComparator.java
@@ -1,12 +1,35 @@
 package org.example.pointloc;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 
-public class EdgeSrcXComparator implements Comparator<Edge> {
+//public class EdgeSrcXComparator implements Comparator<Edge> {
+//    @Override
+//    public int compare(Edge o1, Edge o2) {
+//        GraphNode o1point = o1.getSrc();
+//        GraphNode o2point = o2.getSrc();
+//        if(o1point.getX() > o2point.getX())
+//            return 1;
+//        if(o1point.getX() < o2point.getX())
+//            return -1;
+//        if(o1point.getY() > o2point.getY())
+//            return 1;
+//        if(o1point.getY() < o2point.getY())
+//            return -1;
+//        return 0;
+//    }
+//}
+
+public class EdgeSrcXComparator implements Comparator<Integer> {
+    private final ArrayList<WeightedEdge> edges;
+
+    public EdgeSrcXComparator(ArrayList<WeightedEdge> edges) {
+        this.edges = edges;
+    }
     @Override
-    public int compare(Edge o1, Edge o2) {
-        GraphNode o1point = o1.getSrc();
-        GraphNode o2point = o2.getSrc();
+    public int compare(Integer o1, Integer o2) {
+        GraphNode o1point = edges.get(o1).getSrc();
+        GraphNode o2point = edges.get(o2).getSrc();
         if(o1point.getX() > o2point.getX())
             return 1;
         if(o1point.getX() < o2point.getX())
